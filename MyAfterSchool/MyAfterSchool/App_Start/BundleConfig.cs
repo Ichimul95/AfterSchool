b768a0f9aaa12d8d1aceac7e4bc0e613e4e99d3b
Ichimul95 <ichimvlad95@gmail.com>
2017-04-18 18:24:34 +0300

    jquery update,created first partial views on home,(header,footer)added slider plugin

--- MyAfterSchool/MyAfterSchool/App_Start/BundleConfig.cs
+++ MyAfterSchool/MyAfterSchool/App_Start/BundleConfig.cs
@@ -8,24 +8,40 @@ namespace MyAfterSchool
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                 .Include("~/Scripts/bootstrap.js")
+                 .Include("~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/Plugins")
+                 .Include("~/Scripts/custom.js")
+                 .Include("~/Assets/banner/js/jquery.themepunch.tools.min.js")
+                 .Include("~/Assets/banner/js/jquery.themepunch.revolution.js")
+                 .Include("~/Assets/banner/js/jquery.themepunch.enablelog.js"));
+            
+            /*-------CSS SECTION--------*/
+
+            bundles.Add(new StyleBundle("~/Plugins/css")
+                .Include("~/Assets/css/color-option1.css")
+                .Include("~/Assets/banner/css/settings.css")
+                .Include("~/Assets/css/style.css")
+                .Include("~/Assets/fonts/font-awesome.min.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                    .Include("~/Content/css/custom.css")
+                    .Include("~/Content/bootstrap.css")
+                    .Include("~/Content/site.css"));
         }
     }
 }
